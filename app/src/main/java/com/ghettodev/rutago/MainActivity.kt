e17e2a567a1cc0a78bf892acfb73f4679d4954c5
Jeronimo <inocencio2004jero@gmail.com>
2026-03-14 22:13:52 -0600

    Primer commit del proyecto RutaGo

---
+++ app/src/main/java/com/ghettodev/rutago/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.ghettodev.rutago
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.enableEdgeToEdge
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+
+        enableEdgeToEdge()
+
+    }
+
+}
